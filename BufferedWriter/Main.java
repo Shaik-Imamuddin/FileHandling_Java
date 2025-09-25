23548175ae5525c5038830f18c9320ccb7c0a24c
Immu <shaikimam37302@gmail.com>
2025-09-25 14:35:42 +0530

    File Handling

---
+++ BufferedWriter/Main.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        FileWriter fw = new FileWriter("sample.txt");
+        BufferedWriter bw = new BufferedWriter(fw);
+        String inp =sc.nextLine();
+        bw.write(inp);
+        bw.newLine();
+        bw.close();
+        System.out.println("Data written successfully..");
+    }
+}
